96d1edb8b2cdc0524970f1c30ff8cfa5ac02fc14
JiangGufan <120249579+JiangGufan@users.noreply.github.com>
2023-11-12 12:03:37 +0800

    Add files via upload
    11.12第一次更新；完成至flask handbook第五节

---
+++ watchlist/app.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 15:47:44 2023
+
+@author: Panda Jiang
+"""
+import os
+import sys
+
+import click
+
+# 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app：
+from flask import Flask, render_template
+from markupsafe import escape
+from flask import url_for
+from faker import Faker
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+    
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
+
+
+# 模型类要声明继承 db.Model
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+
+
+# 用 app.route() 装饰器来为处理函数绑定对应的 URL
+# 在浏览器访问这个 URL 时，触发这个函数，获取返回值，并把返回值显示到浏览器窗口
+@app.route('/index')
+@app.route('/home')
+# 填入 app.route() 装饰器的第一个参数是 URL 规则字符串，这里的 /指的是根地址
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
+# escape() 函数对 name 变量进行转义处理，比如把 < 转换成 &lt,返回响应时浏览器就不会把它们当做代码执行
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
+
+name = 'Jiang Panda'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+# 使用fake类创建虚拟名字
+fake = Faker()
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=fake.name(), movies=movies)
+
+
+# if __name__ == '__main__':
+#     app.run()
