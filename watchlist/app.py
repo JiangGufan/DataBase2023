360ea259f7038cb5f4f73ce00452df87bec89163
JiangGufan <120249579+JiangGufan@users.noreply.github.com>
2023-11-13 19:23:04 +0800

    11.13 第二次更新，完成至FlaskHandbook第七章

--- watchlist/app.py
+++ watchlist/app.py
@@ -9,10 +9,8 @@ import sys
 
 import click
 
-# 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app：
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash
 from markupsafe import escape
-from flask import url_for
 from faker import Faker
 from flask_sqlalchemy import SQLAlchemy
 
@@ -21,10 +19,13 @@ if WIN:  # 如果是 Windows 系统，使用三个斜线
     prefix = 'sqlite:///'
 else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
-    
+
+# 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app：    
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 密钥的值在开发时可以随便设置。基于安全的考虑，在部署时应该设置为随机字符，且不应该明文写在代码里
+app.config['SECRET_KEY'] = 'dev'
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 
 
@@ -39,9 +40,41 @@ class Movie(db.Model):  # 表名将会是 movie
     title = db.Column(db.String(60))  # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
+# Flask 的一个装饰器，用于创建自定义的命令行接口（CLI）命令
+# 该装饰器允许定义特定的函数作为命令行命令，比如'forge'函数，可通过Flask应用的命令行接口执行，如'flask forge'
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+    # 使用fake类创建虚拟名字
+    fake = Faker()
+    # 全局的两个变量移动到这个函数内
+    name = fake.name()
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
 
-@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
-@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+    
+@app.cli.command()  
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项 如果增加--drop则True，需要执行drop
+# 用来初始化数据库内容
 def initdb(drop):
     """Initialize the database."""
     if drop:  # 判断是否输入了选项
@@ -77,27 +110,80 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
 
-name = 'Jiang Panda'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
 
-# 使用fake类创建虚拟名字
-fake = Faker()
 
-@app.route('/')
+
+
+# 一种错误页面的代码呈现
+# @app.errorhandler(404)  # 传入要处理的错误代码
+# def page_not_found(e):  # 接受异常对象作为参数
+#     user = User.query.first()
+#     return render_template('404.html', user=user), 404  # 返回模板和状态码
+
+@app.context_processor
+# 是 Flask 中的一个装饰器，用来注册一个上下文处理函数，可以在所有模板中使用
+# 这里使所有调用user的templates都呈现User.query.first()
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404) # 传入要处理的错误代码
+def page_not_found(e): # 接受异常对象作为参数
+    return render_template('404.html'), 404
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', name=fake.name(), movies=movies)
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        # session 用来在请求间存储数据，它会把数据签名后存储到浏览器的Cookie中
+        flash('Item created.')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
+
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
 
 
-# if __name__ == '__main__':
-#     app.run()
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))
+
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', movie=movie)
+
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+    app.run()
